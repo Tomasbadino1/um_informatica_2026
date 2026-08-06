36845097f75ab85717c31d29ca42639ab3b26ae5
Tomas Badino <tbadino07@gmail.com>
2026-08-05 23:02:51 -0300

    tp_2_ejercicio_B3

---
+++ TP2_Badino_Tomas/ejercicioB3.py
@@ -0,0 +1,37 @@
+#Codificador César
+
+def cifrar(texto, n):
+    cifrado = []
+    for letra in texto:
+        if letra.isupper():
+            codigo = ord(letra) - ord("A")
+            nuevo_codigo = (codigo + n) % 26 + ord("A")
+            cifrado.append(chr(nuevo_codigo))
+            
+        elif letra.islower():
+            codigo = ord(letra) - ord("a")
+            nuevo_codigo = (codigo + n) % 26 + ord("a")
+            cifrado.append(chr(nuevo_codigo))
+        else:
+            cifrado.append(letra)
+    return "".join(cifrado)
+
+
+def descifrar(texto, n):
+    descifrado = []
+    for letra in texto:
+        if letra.isupper():
+            codigo = ord(letra) - ord("A")
+            nuevo_codigo = (codigo - n) % 26 + ord("A")
+            descifrado.append(chr(nuevo_codigo))
+            
+        elif letra.islower():
+            codigo = ord(letra) - ord("a")
+            nuevo_codigo = (codigo - n) % 26 + ord("a")
+            descifrado.append(chr(nuevo_codigo))
+        else:
+            descifrado.append(letra)
+    return "".join(descifrado)
+
+resultado = cifrar("Hola mundo", 3)
+#descifrar("def ABC", 3)
