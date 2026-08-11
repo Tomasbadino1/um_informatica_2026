78cf87cd84e8db8352116b338b7f0a7bacf6e469
Tomas Badino <tbadino07@gmail.com>
2026-08-11 10:48:15 -0300

    Clases - Correccion README.md

---
+++ Clases/clase03/errores.py
@@ -0,0 +1,21 @@
+#Errores más comunes a la hora de escribir el codigo:
+#SyntaxError: Errores de gramatica, faltan símbolos clave como comillas, paréntesis o dos puntos (:).
+
+#IndentationError: Los espacios o tabulaciones del código están mal alineados.
+
+#NameError: Usas una variable o función que todavía no definiste.
+
+#TypeError: Mezclas tipos de datos incompatibles, como sumar un número con un texto. Los strings son inmutables
+
+#ValueError: Una función recibe un dato del tipo correcto pero con un valor absurdo o imposible de procesar 
+#(ej. convertir letras a entero).
+
+#ZeroDivisionError: Intentas dividir un número entre cero.
+
+#IndexError: Buscas un número de posición (índice) que no existe en una lista.
+
+#KeyError: Intentas buscar una llave que no existe dentro de un diccionario.
+
+#AttributeError: Pides un atributo o función que el objeto no posee.
+
+#FileNotFoundError: Intentas abrir un archivo que no se encuentra en esa ruta.
--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# TP de Informática
+# Informática 1
 
-Este repositorio está diseñado para trabajos prácticos de la materia Informática, organizados por entregas y ejercicios.
+Este repositorio está diseñado para lo trabajado durante el año en Informática 1, clases y trabajos prácticos de la materia, organizados por entregas y ejercicios.
 
 ## ¿Qué encontrarás aquí?
 
+- `Clases/` - Material de clase organizado por número de clase con ejemplos y ejercicios prácticos.
 - `TP1_Badino_Tomas/` - Primer trabajo práctico con ejercicios numerados del 1 al 9.
 - `TP2_Badino_Tomas/` - Segundo trabajo práctico con ejercicios numerados del 1 al 7 y ejercicios bonus `B1`, `B2`, `B3`.
 
